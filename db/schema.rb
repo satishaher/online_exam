df2a927bfddef4cef307da409630dfe8866bbb11
satishaher <satishg.aher@gmail.com>
2017-01-22 15:47:33 +0530

    teacher students added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121072029) do
+ActiveRecord::Schema.define(version: 20170122101206) do
 
   create_table "users", force: true do |t|
     t.string   "user_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170121072029) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "type"
   end
 
 end
