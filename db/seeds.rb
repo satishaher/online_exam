92856577f1f911bcf70edd8355caf1a4911ff5c1
satishaher <satishg.aher@gmail.com>
2017-01-24 12:52:19 +0530

    seed teacher

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Teacher.create(user_name: 'teacher1', first_name: 'Satish', last_name: 'Aher', password: 'satish123')
